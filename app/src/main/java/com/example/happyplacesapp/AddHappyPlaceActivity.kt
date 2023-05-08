f3b597533feec8c80b36448f58ee283f98d459a3
Khawaja fakhar <fakhar.oraxtech@gmail.com>
2023-05-08 17:26:05 +0500

    Added UI for the Add place activity

--- app/src/main/java/com/example/happyplacesapp/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/example/happyplacesapp/AddHappyPlaceActivity.kt
@@ -2,10 +2,18 @@ package com.example.happyplacesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class AddHappyPlaceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
+
+        val toolbarAddPlace = findViewById<Toolbar>(R.id.toolbar_add_place)
+        setSupportActionBar(toolbarAddPlace)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbarAddPlace.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
