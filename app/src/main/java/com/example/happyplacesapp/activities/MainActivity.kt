a2c6e9d50996dbbda370dfd0e33a53b6d569f3f8
Khawaja fakhar <fakhar.oraxtech@gmail.com>
2023-05-10 11:25:00 +0500

    connected adpater with recycler to show lists

--- app/src/main/java/com/example/happyplacesapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/happyplacesapp/activities/MainActivity.kt
@@ -4,17 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.happyplacesapp.R
 import com.example.happyplacesapp.activities.AddHappyPlaceActivity
+import com.example.happyplacesapp.adapters.HappyPlacesAdapter
 import com.example.happyplacesapp.database.DatabaseHandler
+import com.example.happyplacesapp.models.HappyPlace
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private var rvRecyclerView: RecyclerView? = null
+    private var tvNoRecord: TextView? =null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var fabAddHappyPlaceActivity:FloatingActionButton = findViewById(R.id.fabAddPlace)
+        rvRecyclerView = findViewById(R.id.rv_main_activity)
+        tvNoRecord = findViewById(R.id.tv_no_places)
+        var fabAddHappyPlaceActivity: FloatingActionButton = findViewById(R.id.fabAddPlace)
         fabAddHappyPlaceActivity.setOnClickListener {
             val intent = Intent(this, AddHappyPlaceActivity::class.java)
             startActivity(intent)
@@ -22,15 +31,28 @@ class MainActivity : AppCompatActivity() {
         getAllPlacesFromDatabase()
     }
 
-    private fun getAllPlacesFromDatabase(){
+    private fun setupHappyPlacesRecyclerView(
+        happyPlaceList:
+        ArrayList<HappyPlace>
+    ) {
+      if(happyPlaceList.size>0)  {
+            tvNoRecord?.visibility =View.GONE
+            rvRecyclerView?.visibility = View.VISIBLE
+            rvRecyclerView?.layoutManager =
+                LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+            val happyPlacesAdapter = HappyPlacesAdapter(this, happyPlaceList)
+            rvRecyclerView?.adapter = happyPlacesAdapter
+        }else{
+          tvNoRecord?.visibility =View.VISIBLE
+          rvRecyclerView?.visibility = View.GONE
+        }
+
+    }
+
+    private fun getAllPlacesFromDatabase() {
         val db = DatabaseHandler(this)
-       val allHappyPlaces = db.getAllHappyPlaces()
-        Log.e("Size",(allHappyPlaces.size > 0).toString())
-        if(allHappyPlaces.size > 0){
-            for(i in allHappyPlaces){
-                Log.e("Title",i.title)
-                Log.e("Description",i.description)
-            }
+        val allHappyPlaces = db.getAllHappyPlaces()
+        setupHappyPlacesRecyclerView(allHappyPlaces)
+
         }
     }
-}
